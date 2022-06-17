8dfee2a4e2304db691f33fe969c6ecdfe11e804a
PellonMaison <marianomaison@hotmail.com>
2022-06-17 14:50:25 -0300

    Created model and migrations Slides

---
+++ src/migrations/20220617174457-create-slides.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Slides', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        type: Sequelize.STRING
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      order: {
+        type: Sequelize.STRING
+      },
+      organizationId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Roles',
+          key: 'id',
+          },
+        },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Slides');
+  }
+};
