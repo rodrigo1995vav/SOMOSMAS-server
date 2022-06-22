461bd2807b98de6221872cf7f4e0c33a4d9fbf21
francogaliotti <francogaliotti@gmail.com>
2022-06-22 11:15:57 -0300

    user Controller import

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const userController = require('../controllers/users-controllers')
 
 router.get("/users");
 router.post("/login", userController.login)
