03aa65049c77fb62dd2b99b562be7cf62ada363e
francogaliotti <francogaliotti@gmail.com>
2022-06-24 16:06:17 -0300

    s3-service created

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const organizationController = require('../controllers/organization-controllers')
+const multer = require('multer')
+const upload = multer({ dest: 'public/' })
 
 router.get("/:id/public", organizationController.getPublicData);
+
+
+router.post("/img", upload.single('image'), async (req, res, next) => {
+    const file = req.file
+    console.log(file)
+})
 
 module.exports = router;
