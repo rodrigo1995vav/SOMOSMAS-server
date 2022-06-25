587c60ea419392f1bcaaa8312ddd4fcad43c54c7
francogaliotti <francogaliotti@gmail.com>
2022-06-25 00:45:58 -0300

    s3 service done

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -1,15 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const organizationController = require('../controllers/organization-controllers')
-const multer = require('multer')
-const upload = multer({ dest: 'public/' })
 
 router.get("/:id/public", organizationController.getPublicData);
-
-
-router.post("/img", upload.single('image'), async (req, res, next) => {
-    const file = req.file
-    console.log(file)
-})
 
 module.exports = router;
