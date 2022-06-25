aacb697681a78e7c1c96156dd26838c9810dfae7
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-06-25 14:59:11 -0500

    Add createNewEntry function to entries controller file

--- src/controllers/entries-controller.js
+++ src/controllers/entries-controller.js
@@ -1,4 +1,5 @@
-const entryService = require('../services/entry-service');
+const entryService   = require('../services/entry-service');
+const { uploadFile } = require('../services/s3-service');
 
 const getNewsEntries = async(req, res) => {
   try {
@@ -11,6 +12,28 @@ const getNewsEntries = async(req, res) => {
   
 }
 
+const createNewEntry = async(req, res) => {
+
+  const { image, ...rest } = req.body;
+
+  // Uploads image to AWS S3 service and extract de key value
+  const { key } = await uploadFile( req.files.file );
+
+  // const key = 'dfe64q3jhasd3jjafrdkj';
+
+  const entrySaved = await entryService.createEntry({
+    ...rest,
+    image: key,
+    type: 'news'
+  })
+
+  res.status(201).json({
+    ok: true,
+    entry: entrySaved
+  })
+}
+
 module.exports = {
-  getNewsEntries
+  getNewsEntries,
+  createNewEntry
 }
