0130778754c5b2cf5752e6eda824637fad4a77c1
francogaliotti <francogaliotti@gmail.com>
2022-07-08 23:52:49 -0300

    File validator fixes

--- src/controllers/testimonial-controller.js
+++ src/controllers/testimonial-controller.js
@@ -1,5 +1,6 @@
 const testimonialService = require('../services/testimonial-service')
 const { uploadFile } = require('../services/s3-service');
+const fileServices = require("../services/fileServices");
 
 
 const getAllTestimonials = async (req, res, next) => {
@@ -7,41 +8,31 @@ const getAllTestimonials = async (req, res, next) => {
     let page = Number(req.params.page)
     let limit = Number(req.params.limit)
     try {
-       
-        const allTestimonials = await testimonialService.getAllTestimonials(limit,page)
+
+        const allTestimonials = await testimonialService.getAllTestimonials(limit, page)
 
         res.json(allTestimonials)
     }
     catch (err) {
-        
+
         res.status(500).json(err)
     }
 
 }
 
-const createNewTestimony = async(req, res) => {
-    // Uploads image to AWS S3 service and extract de key value
-    const imageFileCheck = async function () {
-        if(req.file){
-            const { key } = await uploadFile( req.file );
-            return key
-        } else {
-            return ""
-        }
+const createNewTestimony = async (req, res) => {
+    try {
+        console.log(req)
+        const image = await fileServices.checkFileAndUpload(req.file)
+        const testimonySaved = await testimonialService.createTestimony({
+            ...req.body,
+            image
+        })
+        res.status(201).json(testimonySaved)
+    } catch (err) {
+        res.status(500).json(err);
     }
-
-    const image = await imageFileCheck()
-  
-    const testimonySaved = await testimonialService.createTestimony({
-      ...req.body,
-      image
-    })
-  
-    res.status(201).json({
-      ok: true,
-      entry: testimonySaved
-    })
-  }
+}
 
 module.exports = {
     getAllTestimonials,
--- src/routes/testimonials.js
+++ src/routes/testimonials.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const testimonialController = require('../controllers/testimonial-controller');
-const multer = require('multer');
 const { validateCreateTestimony } = require("../middlewares/validations/Testimonial");
-const upload = multer({ dest: 'uploads/' })
+const validateFile = require("../middlewares/file-validator");
+
 
 
 router.get("/:limit/:page", testimonialController.getAllTestimonials)
 
 router.post("/",
-  upload.single('image'),
+  validateFile,
   validateCreateTestimony,
   testimonialController.createNewTestimony);
 
