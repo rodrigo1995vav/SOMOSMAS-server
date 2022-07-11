b2646096efb98d5b276fd41ea2e661cf402bc541
Leandro Medina <medinaleandron@gmail.com>
2022-07-11 01:11:36 -0300

    Update CategoryDataValidation.js

--- src/middlewares/validations/CategoryDataValidation.js
+++ src/middlewares/validations/CategoryDataValidation.js
@@ -3,8 +3,12 @@
 const { body } = require('express-validator')
 
 
+
 module.exports = [
     body('name')
         .notEmpty().withMessage('Debe completar el compo nombre')
         .isString().withMessage('El campo nombre debe contener texto'),
+    body('description')
+       
+        
 ]
