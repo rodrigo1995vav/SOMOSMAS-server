7a07fcfcb36eff3fede10e574a7531a10ad6ebb2
MarcosSWeis <marcosweis01@gmail.com>
2022-07-11 22:17:25 -0300

    add middleware

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const categoryControllers = require("../controllers/category-cntroller");
+const checkAdminUser = require("../middlewares/authentication/check-admin-user");
 const router = express.Router();
 
-router.get('/',categoryControllers.getListCategory)
+router.get('/', checkAdminUser ,categoryControllers.getListCategory)
 
 module.exports = router
