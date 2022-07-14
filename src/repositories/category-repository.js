dbf7fbc5174f7391394a58560d34069dada671e7
MarcosSWeis <marcosweis01@gmail.com>
2022-07-14 16:43:30 -0300

    attribute id categories

--- src/repositories/category-repository.js
+++ src/repositories/category-repository.js
@@ -7,7 +7,7 @@ const saveCategory = async (newCategory) => {
 
     // if newCategory id is not null, it will update the category. If id is null it will create a new register
 
-    const instancedCategory = Category.build( newCategory,{ isNewRecord: !newCategory.id } )
+    const instancedCategory = Category.build(newCategory, { isNewRecord: !newCategory.id })
 
     await instancedCategory.save()
 
@@ -17,15 +17,15 @@ const saveCategory = async (newCategory) => {
 
 
 
-const listAllCategory = async(page , limit)=>{
-    const offset = page*limit
-    const {count, rows} =await Category.findAndCountAll({
-        attributes: ['name'],
-        raw:true,        
-        offset:offset,
-        limit 
+const listAllCategory = async (page, limit) => {
+    const offset = page * limit
+    const { count, rows } = await Category.findAndCountAll({
+        attributes: ['id', 'name'],
+        raw: true,
+        offset: offset,
+        limit
     })
-  return { total_categories:count , current_page:page+1 , total_pages:Math.ceil(count/limit) , categories:rows}
+    return { total_categories: count, current_page: page + 1, total_pages: Math.ceil(count / limit), categories: rows }
 }
 
 
