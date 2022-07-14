afa9eb8ea9e817f1c752e8f897f2bd3e5dfed284
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-07-14 17:24:38 -0300

    Merge

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const categoryControllers = require("../controllers/category-cntroller");
+const categoryControllers = require("../controllers/category-controller");
 const checkAdminUser = require("../middlewares/authentication/check-admin-user");
 const router = express.Router();
 
