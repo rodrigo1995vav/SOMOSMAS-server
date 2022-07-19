859ad196e123c3e7a6123bc33fc3ee53746ed956
francogaliotti <francogaliotti@gmail.com>
2022-07-19 10:12:35 -0300

    member controller name fix

--- src/controllers/member-controller.js
+++ src/controllers/member-controller.js
@@ -16,6 +16,20 @@ const updateMember = async (req, res, next) => {
     }
 }
 
+const getListMember = async (req,res)=>{
+    try {
+    const { query } = req;
+        
+     const members= await memberService.getListAllMembers(query)
+
+     res.status(200).json(members) 
+    } catch (err) {
+        res.status(500).json(err.message)
+        console.log(err)
+    }
+}
+
 module.exports ={
-    updateMember
+    updateMember,
+    getListMember
 }
