0861be4ecf6511d95504d03444c5f2417a010556
rodrigo1995vav <rodrigo1995val@hotmail.com>
2022-07-24 17:38:49 -0300

    added key to entries image

--- src/seeders/20220707000159-entries.js
+++ src/seeders/20220707000159-entries.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     {
       name: 'Demo news 2',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: '9fa679ef2b8cca0fb881a37ff711aa12',
       content: 'This is the demo news number 2',
       type: 'news',
       categoryId: 1,
@@ -31,7 +31,7 @@ module.exports = {
     },
     {
       name: 'Demo news 3',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: '079896e23bbbd35072ceba400dcc0b7b',
       content: 'This is the demo news number 3',
       type: 'news',
       categoryId: 1,
@@ -40,7 +40,7 @@ module.exports = {
     },
     {
       name: 'Demo news 4',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: 'cd20b6be88c9d191b50513885406aa0d',
       content: 'This is the demo news number 4',
       type: 'news',
       categoryId: 1,
@@ -49,7 +49,7 @@ module.exports = {
     },
     {
       name: 'Demo event 5',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: '4913db44a2218b9962175c497aaeba35',
       content: 'This is the demo event number 5',
       type: 'event',
       categoryId: 1,
@@ -58,7 +58,7 @@ module.exports = {
     },
     {
       name: 'Demo activity 6',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: '59590b6bc1614a14e4c0b129dbf62346',
       content: 'This is the demo activity number 6',
       type: 'event',
       categoryId: 1,
@@ -85,7 +85,7 @@ module.exports = {
     },
     {
       name: 'Demo activity 9',
-      image: '1b84c3f95c84088e0fa6a4ff54f9a90e',
+      image: '00f802f29e60f4a087e94011248eb943',
       content: 'This is the demo activity number 9',
       type: 'news',
       categoryId: 1,
