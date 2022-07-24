539dcc227804c2dc3271f5f95cfaac68787ad589
MarcosSWeis <marcosweis01@gmail.com>
2022-07-24 20:16:00 -0300

    testimonial association

--- src/migrations/20220704141359-create-testimonial.js
+++ src/migrations/20220704141359-create-testimonial.js
@@ -1,33 +1,40 @@
 'use strict';
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('testimonials', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      image: {
-        type: Sequelize.STRING
-      },
-      content: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('testimonials');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('testimonials', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING
+            },
+            image: {
+                type: Sequelize.STRING
+            },
+            content: {
+                type: Sequelize.STRING
+            },
+            userId: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'users',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('testimonials');
+    }
 };
--- src/models/testimonial.js
+++ src/models/testimonial.js
@@ -1,25 +1,26 @@
 'use strict';
 const {
-  Model
+    Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Testimonial extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    class Testimonial extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            Testimonial.belongsTo(models.User, { as: 'user' })
+        }
     }
-  }
-  Testimonial.init({
-    name: DataTypes.STRING,
-    image: DataTypes.STRING,
-    content: DataTypes.STRING
-  }, {
-    sequelize,
-    tableName: 'testimonials',
-  });
-  return Testimonial;
+    Testimonial.init({
+        name: DataTypes.STRING,
+        image: DataTypes.STRING,
+        content: DataTypes.STRING,
+        // userId: DataTypes.INTEGER
+    }, {
+        sequelize,
+        tableName: 'testimonials',
+    });
+    return Testimonial;
 };
