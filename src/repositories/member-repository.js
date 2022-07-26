cb8712795b25ed24a4b6d18f214df87e391cfb8f
francogaliotti <francogaliotti@gmail.com>
2022-07-26 20:22:00 -0300

    member repository uodate

--- src/repositories/member-repository.js
+++ src/repositories/member-repository.js
@@ -1,14 +1,18 @@
 const { Members, Role_ong, sequelize } = require('../models')
 
-const getAllMembers = async (page, limit) => {
-    const offset = page * limit;
+const updateMember = async (newContent) => {
+    const memberUpdated = await Members.update(newContent,{where:{id: newContent.id}})
+    if (memberUpdated[0]===0){
+        
+        return null
+    }
+    return "Member updated"
+}
 
-    const { count, rows } = await Members.findAndCountAll({
-        include: [{
-            model: Role_ong,
-            attributes: ['role_ong']
-        }],
-        offset: offset,
+const getAllMembers = async(page,limit)=>{
+    const offset = page*limit;
+   const {count, rows }= await Members.findAndCountAll({
+        offset : offset,
         limit: limit
     })
     return { total_members: count, total_pages: Math.ceil(count / limit), current_page: page + 1, members: rows }
