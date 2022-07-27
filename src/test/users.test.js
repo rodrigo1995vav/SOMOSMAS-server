b2fdbed8bbe6dda07c7b3ceaa4d0755679f5ab64
francogaliotti <francogaliotti@gmail.com>
2022-07-27 17:18:33 -0300

    user put methods tested

--- src/test/users.test.js
+++ src/test/users.test.js
@@ -17,6 +17,21 @@ const login = () => {
     return token
 }
 
+const saveUser = async () => {
+    const user = User.build({
+        "firstName": "Jasmine",
+        "lastName": "Chellam",
+        "email": "jchellam0@cbsnews.com",
+        "password": "lJGRQsZ9Pnx",
+        "image": "8cee83bafdb828b1af7bbf669e09deb2",
+        "roleId": 2,
+        "createdAt": "2022-01-20 21:45:33",
+        "updatedAt": "2022-03-12 13:11:07"
+    })
+    await user.save();
+    return user;
+};
+
 describe('GET /users testing', () => {
     it('returns a paginated list of users', async () => {
         const token = login();
@@ -34,8 +49,48 @@ describe('GET /users testing', () => {
     })
 })
 
-describe('PUT /users testing', () => {
+describe('PUT /users/updateProfile testing', () => {
+    it('updates a User', async () => {
+        const user = await saveUser()
+        const updateResponse = await request(app)
+            .put('/users/updateProfile')
+            .send({
+                firstName:"new firstName",
+                lastName: "new lastName",
+                email: "new email",
+                id: user.id
+            })
+            .expect(200)
+    })
+})
 
+describe('PUT /users/update testing', () => {
+    it('updates a User', async () => {
+        const user = await saveUser()
+        const token = login();
+        const updateResponse = await request(app)
+            .put('/users/update')
+            .set('Authorization', 'bearer ' + token)
+            .send({
+                firstName:"new firstName",
+                lastName: "new lastName",
+                email: "new email",
+                id: user.id
+            })
+            .expect(200)
+    })
+    it('can only be accessed if the user is logged as an admin', async () => {
+        const user = await saveUser()
+        const updateResponse = await request(app)
+            .put('/users/update')
+            .send({
+                firstName:"new firstName",
+                lastName: "new lastName",
+                email: "new email",
+                id: user.id
+            })
+            .expect(401)
+    })
 })
 
 describe('DELETE /users testing', () => {
